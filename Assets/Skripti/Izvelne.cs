02e1e3044374e14cbc226a6db53ffcd6700be05c
Kristers Emils Cernisevs <kristers.cernisevs@gmail.com>
2022-03-30 19:04:33 +0300

    Pievienota Sākuma izvēlne

---
+++ Assets/Skripti/Izvelne.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// Importē lai varētu ielādēt ainas
+using UnityEngine.SceneManagement;
+
+public class Izvelne : MonoBehaviour {
+
+	// Lai ar pogas nospiešanu mainītos uz galveno ainu
+	public void uzGalveno(){
+		SceneManager.LoadScene ("Pilseta", LoadSceneMode.Single);
+
+	}
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
